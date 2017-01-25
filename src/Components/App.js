edd87c4d1e38971f31bd2191562acb5d24cedad6
Christopher Pruitt <pruicr@gmail.com>
2017-01-25 12:42:04 -0800

    get data from api and output to page

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+import Output from './Output'
 
 class App extends Component {
-  componentDidMount() {
-    console.log('React running')
+  constructor(props) {
+    super(props)
+    this.state = {
+      paras: 4,
+      html: true,
+      text: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getText()
+  }
+
+  getText() {
+    axios.get('http://hipsterjesus.com/api?paras=' + this.state.paras + '&html=' + this.state.html)
+      .then((res) => {
+        this.setState({text: res.data.text}, () => {
+          console.log(this.state)
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   render() {
     return (
       <div>
-        App Component
+        <h1>Text Generator</h1>
+        <Output value={this.state.text}/>
       </div>
     )
   }
