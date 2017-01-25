4db59e43946697223eeb4a0a13ce3e01603b9e33
Christopher Pruitt <pruicr@gmail.com>
2017-01-25 12:10:02 -0800

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  componentDidMount() {
+    console.log('React running')
+  }
+
+  render() {
+    return (
+      <div>
+        App Component
+      </div>
+    )
+  }
+}
+
+export default App
